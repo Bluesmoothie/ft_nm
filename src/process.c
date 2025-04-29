0d5094cbe1ad3e3c154f645567591947e2781ee0
Yves Gille <ygille@student.42lyon.fr>
2025-04-29 21:44:26 +0200

    WIP symbols

--- src/process.c
+++ src/process.c
@@ -6,19 +6,21 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 19:43:36 by ygille            #+#    #+#             */
-/*   Updated: 2025/04/29 20:49:22 by ygille           ###   ########.fr       */
+/*   Updated: 2025/04/29 21:44:14 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "nm.h"
 
 static void	get_section_header(t_context *ctx);
-static void	get_symbol_sections(t_context *ctx);
+static void	process_symbol_sections(t_context *ctx);
+static void	list_symbols(t_context *ctx, size_t offset, size_t size);
+static void	print_symbol(size_t value, unsigned char bind, unsigned char type);
 
 void	process(t_context *ctx)
 {
 	get_section_header(ctx);
-	get_symbol_sections(ctx);
+	process_symbol_sections(ctx);
 }
 
 static void	get_section_header(t_context *ctx)
@@ -40,7 +42,7 @@ static void	get_section_header(t_context *ctx)
 	}
 }
 
-static void	get_symbol_sections(t_context *ctx)
+static void	process_symbol_sections(t_context *ctx)
 {
 	Elf32_Shdr	*section32;
 	Elf64_Shdr	*section64;
@@ -50,10 +52,8 @@ static void	get_symbol_sections(t_context *ctx)
 		section32 = ctx->file + ctx->sh_offset;
 		for (size_t i = 0;i < ctx->sh_num;i++)
 		{
-			if (section32->sh_type == SHT_SYMTAB)
-				printf("Found sym table at %u\n", section32->sh_offset);
-			else if (section32->sh_type == SHT_DYNSYM)
-				printf("Found dyn sym table at %u\n", section32->sh_offset);
+			if (section32->sh_type == SHT_SYMTAB || section32->sh_type == SHT_DYNSYM)
+				list_symbols(ctx, section32->sh_offset, section32->sh_size);
 			section32++;
 		}
 	}
@@ -62,11 +62,59 @@ static void	get_symbol_sections(t_context *ctx)
 		section64 = ctx->file + ctx->sh_offset;
 		for (size_t i = 0;i < ctx->sh_num;i++)
 		{
-			if (section64->sh_type == SHT_SYMTAB)
-				printf("Found sym table at %lu\n", section64->sh_offset);
-			else if (section64->sh_type == SHT_DYNSYM)
-				printf("Found dyn sym table at %lu\n", section64->sh_offset);
+			if (section64->sh_type == SHT_SYMTAB || section64->sh_type == SHT_DYNSYM)
+				list_symbols(ctx, section64->sh_offset, section64->sh_size);
 			section64++;
 		}
 	}
+}
+
+static void	list_symbols(t_context *ctx, size_t offset, size_t size)
+{
+	Elf32_Sym	*sym32;
+	Elf64_Sym	*sym64;
+
+	if (ctx->filetype == ELFCLASS32)
+	{
+		sym32 = ctx->file + offset;
+		while (size)
+		{
+			print_symbol(sym32->st_value, ELF32_ST_BIND(sym32->st_info), ELF32_ST_TYPE(sym32->st_info));
+			sym32++;
+			size -= sizeof(Elf32_Sym);
+		}
+	}
+	else if (ctx->filetype == ELFCLASS64)
+	{
+		sym64 = ctx->file + offset;
+		while (size)
+		{
+			print_symbol(sym64->st_value, ELF64_ST_BIND(sym64->st_info), ELF64_ST_TYPE(sym64->st_info));
+			sym64++;
+			size -= sizeof(Elf64_Sym);
+		}
+	}
+}
+
+static void	print_symbol(size_t value, unsigned char bind, unsigned char type)
+{
+	char	id;
+
+	switch (type)
+	{
+	case STT_OBJECT:
+		id = 'r';
+		break;
+	case STT_FUNC:
+		id = 't';
+		break;
+	default:
+		id = type + 48;
+		break;
+	}
+	if (bind == STB_GLOBAL)
+		id = ft_toupper(id);
+	if (!value)
+		id = 'U';
+	printf("%lx %c %d\n", value, id, bind);
 }
